4d1f9225fdb32e5fbefa91848ccecb6ba0834c58
Christian Olms <git@christianolms.de>
2016-02-02 09:59:43 +0100

    removed comments

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,15 +12,10 @@ import State.State;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
-//class StateChangeRecorder{
-//	  @Subscribe public void recordCustomerChange(StateChangeEvent e) {
-//		    System.out.println("Transition from " + e.getOldState().toString() + " to "+e.getNewState().toString());
-//		  }
-//}
+
 public class Main {
 	public static void main(String[] args) {
-//		LCD.drawString("Running ...", 0, 0);
-//		Output.printOnDisplay("Running...");
+
 //		HAL hal = new HAL();
 		IHAL hal = new DefaultHAL(){
 			@Override
@@ -33,7 +28,6 @@ public class Main {
 
 		ArrayList<Behavior> behaviors = new ArrayList<Behavior>();
 		SharedState sharedState = new SharedState(State.LineSearch);
-//		sharedState.register(new StateChangeRecorder());
 		
 		behaviors.add(new LineSearchBehavior(sharedState, hal));
 		behaviors.add(new DisplayTestStateBehavior(sharedState, hal));
