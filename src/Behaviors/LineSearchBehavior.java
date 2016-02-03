63a049438b1a920793f3fa1ac0bd04e972c0db90
Christian Olms <git@christianolms.de>
2016-02-03 10:44:39 +0100

    Code cleanup

--- src/Behaviors/LineSearchBehavior.java
+++ src/Behaviors/LineSearchBehavior.java
@@ -17,7 +17,6 @@ import java.io.*;
 public class LineSearchBehavior extends StateBehavior {
 	public static final float THRESHOLD = 0.1f;
 	public static final int MEAN_WINDOW = 5;
-	public static final boolean KEEP_MEASUREMENTS = false;
 
 	// TODO: update this as soon as we have proper handling in the HAL
 	public static final int EXPLORATION_ANGLE_DIFF = 25;
@@ -27,112 +26,99 @@ public class LineSearchBehavior extends StateBehavior {
 	private Port port;
 
 	private boolean suppressed;
-
+	private MeanFilter meanFilter;
+	private SampleProvider sampleProvider;
+	private float[] meanBuffer;
+	
 	public LineSearchBehavior(SharedState sharedState, IHAL hal, Port port) {
 		super(sharedState, hal);
 		this.port = port;
 		this.suppressed = false;
+		this.halInit();
 	}
-	private BufferedWriter openWriter(String filename) throws IOException{
-		if(KEEP_MEASUREMENTS)
-			filename+= System.currentTimeMillis()+".bak";
-		File file = new File(filename);
+	private void halInit(){
+		EV3ColorSensor sensor = this.hal.getColorSensor();
+		sampleProvider = sensor.getRedMode();
+		this.meanFilter = new MeanFilter(sampleProvider, LineSearchBehavior.MEAN_WINDOW);
+		this.meanBuffer = new float[meanFilter.sampleSize()];
 
-		BufferedWriter out = new BufferedWriter(new FileWriter(file));
-	      return out;
 	}
 	@Override
 	public void action() {
-		EV3ColorSensor sensor = this.hal.getColorSensor();
-		SampleProvider sampleProvider = sensor.getRedMode();
-		MeanFilter meanFilter = new MeanFilter(sampleProvider, LineSearchBehavior.MEAN_WINDOW);
-		float[] meanBuffer = new float[meanFilter.sampleSize()];
 		float[] valueBuffer = new float[sampleProvider.sampleSize()];
 		// float[] fileSample = new float[sampleProvider.sampleSize()];
 		// TODO: implement handling the barcode
-		BufferedWriter out; // declare outside the try block
-
-		try {
-			out = openWriter("sensor.log");
-
 			int counter = 1;
 			Direction direction = Direction.LEFT;
+
 			while (!this.suppressed) {
 				// Do not sample too often.
 				Delay.msDelay(LineSearchBehavior.LOOP_DELAY);
-//				this.writeToLogFile(valueBuffer[0], out);
-
-				if (this.isOnLine(meanFilter, meanBuffer, sampleProvider, valueBuffer, out)) {
+				if (this.isOnLine()) {
 					// Drive forward and reset search strategy values for
 					// potential later use.
 					counter = 1;
-					direction = Direction.LEFT;
+//					direction = Direction.LEFT;
+					direction = Utils.drawDirection();
 					this.hal.forward();
 
-					continue;
+//					continue;
 				}
+				else{
+	
+					// Okay, we're not on the line anymore. Start search strategy.
+					// We first
+					// turn EXPLORATION_ANGLE_DIFF to the left, then the same amount
+					// to the right
+					// (relative to the start position), then 2 *
+					// EXPLORATION_ANGLE_DIFF to the
+					// left, ... and so on until we find the line.
 
-				//Random directory
-				direction = Utils.drawBoolean() ? Direction.LEFT: Direction.RIGHT;
-				// Okay, we're not on the line anymore. Start search strategy.
-				// We first
-				// turn EXPLORATION_ANGLE_DIFF to the left, then the same amount
-				// to the right
-				// (relative to the start position), then 2 *
-				// EXPLORATION_ANGLE_DIFF to the
-				// left, ... and so on until we find the line.
+					//compute the angle to rotate about
+					final int angle_val = counter * LineSearchBehavior.EXPLORATION_ANGLE_DIFF;
+					//Get the right direction for the turn
+					final int turn_angle = direction.equals(Direction.RIGHT)? angle_val : -angle_val;
+					//rotate
+					this.hal.rotate(turn_angle, true);
+					//Rotate until Until we have seen the line again
 
-				int turn_angle = counter * LineSearchBehavior.EXPLORATION_ANGLE_DIFF;
-				if (direction.equals(Direction.RIGHT)) {
-					turn_angle = -turn_angle;
-				}
-				this.hal.rotate(turn_angle, true);
-				while (!this.suppressed && this.hal.motorsAreMoving()) {
-					if (this.isOnLine(meanFilter, meanBuffer, sampleProvider, valueBuffer, out)) {
-						//Overdrive
-						this.hal.rotate(turn_angle, true);
-						// We've found the line, stop moving.
-						this.hal.stop();
-						break;
+					while (!this.suppressed && this.hal.motorsAreMoving()) {
+						if (this.isOnLine()) {
+							//Overdrive
+							this.hal.rotate(turn_angle, true);
+							Delay.msDelay(LineSearchBehavior.LOOP_DELAY / 10);
+							// We've found the line, stop moving.
+							this.hal.stop();
+							break;
+						}
+
+						// Again, do not sample too often here.
+						Delay.msDelay(LineSearchBehavior.LOOP_DELAY / 10);
 					}
+					//invert direction and increase counter: In the next step explore the other direction
+					direction = Direction.changeDirection(direction);
+					counter++;
 
-					// Again, do not sample too often here.
-					Delay.msDelay(LineSearchBehavior.LOOP_DELAY / 10);
 				}
-				
-				direction = Direction.changeDirection(direction);
-				counter++;
 
 			}
-//			out.flush();
-			out.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    finally{
-		sensor.close();
-	      }
+
 	}
 
-	private boolean isOnLine(MeanFilter meanFilter, float[] meanBuffer, SampleProvider sampleProvider,
-			float[] valueBuffer, BufferedWriter out) throws IOException {
-		meanFilter.fetchSample(meanBuffer, 0);
-		float currentMean = meanBuffer[0];
-//		float currentValue = valueBuffer[0];
+	private boolean isOnLine() {
+
+		float currentMean = getMeanSensorValue();
 		boolean isOnLine = currentMean > LineSearchBehavior.THRESHOLD;
 		LCD.drawString("isOnLine: " + isOnLine, 0, 0);
 		LCD.drawString("currentMean: " + currentMean, 0, 1);
 		return isOnLine;
 	}
-
-	private void writeToLogFile(float sample, BufferedWriter out) throws IOException {
-	    out.write(Float.toString(sample));
-	    out.newLine();
+	private float getMeanSensorValue(){
+		this.meanFilter.fetchSample(meanBuffer, 0);
+		float currentMean = this.meanBuffer[0];
+		return currentMean;
 	}
+
 	@Override
 	State getTargetState() {
 		return State.LineSearch;
--- src/Behaviors/Utils.java
+++ src/Behaviors/Utils.java
@@ -4,4 +4,7 @@ public class Utils {
 	public static boolean drawBoolean(){
 		return Math.random()<=0.5;
 	}
+	public static Direction drawDirection(){
+		return Utils.drawBoolean() ? Direction.LEFT: Direction.RIGHT;
+	}
 }
