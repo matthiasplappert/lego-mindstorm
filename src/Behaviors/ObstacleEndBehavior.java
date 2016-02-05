5310b6293dacfdc2a408885fcfcc729738c3a0cf
Matthias Plappert <matthiasplappert@me.com>
2016-02-05 17:22:54 +0100

    Ensure stopped state

--- src/Behaviors/ObstacleEndBehavior.java
+++ src/Behaviors/ObstacleEndBehavior.java
@@ -33,6 +33,7 @@ public class ObstacleEndBehavior extends StateBehavior {
 		// This method starts with the following assumption: The previous running
 		// code drove the robot until it can barely see the line and then immediately stops.
 		// So what we do now is back up a bit, then perform a barcode scan and hopefully see two lines.
+		this.hal.stop();
 		this.hal.resetGyro();
 		this.hal.resetLeftTachoCount();
 		this.hal.resetRightTachoCount();
