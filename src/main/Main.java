b835c052d3c6ecde8e7ccb030db00130f95c1a32
Christian Olms <git@christianolms.de>
2016-02-05 10:57:46 +0100

    rename to maze

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,14 +5,13 @@ import java.util.List;
 
 import Behaviors.BossBehaviour;
 import Behaviors.BridgeBehaviour;
-import Behaviors.DrivebyBehaviour;
 import Behaviors.ElevatorBehaviour;
 import Behaviors.FreeTrackBehaviour;
 import Behaviors.HangingBridgeBehaviour;
 import Behaviors.LineSearchBehavior;
 import Behaviors.MazeBehaviour;
-import Behaviors.SensorDataBehaviour;
 import Behaviors.RollBoxBehaviour;
+import Behaviors.SensorDataBehaviour;
 import Behaviors.ShutdownBehavior;
 import HAL.HAL;
 import HAL.IHAL;
@@ -77,7 +76,7 @@ public class Main {
 		behaviors.add(new MazeBehaviour(sharedState, hal));
 		behaviors.add(new SensorDataBehaviour(sharedState, hal));
 		behaviors.add(new RollBoxBehaviour(sharedState, hal));
-		behaviors.add(new DrivebyBehaviour(sharedState, hal));
+		behaviors.add(new MazeBehaviour(sharedState, hal));
 		
 		// WARNING: always keep this as the last element since it allows us to exit from the program. 
 		behaviors.add(new ShutdownBehavior());
