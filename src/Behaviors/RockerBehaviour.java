559c3a154571140b806d16e9e698b8484de7e9b9
Matthias Plappert <matthiasplappert@me.com>
2016-02-09 14:19:09 +0100

    Implement new ultrasonic-based rocker

--- src/Behaviors/RockerBehaviour.java
+++ src/Behaviors/RockerBehaviour.java
@@ -1,5 +1,6 @@
 package Behaviors;
 
+import HAL.DistanceSensorPosition;
 import HAL.IHAL;
 import HAL.Speed;
 import State.SharedState;
@@ -12,20 +13,144 @@ public class RockerBehaviour extends StateBehavior {
 	public RockerBehaviour(SharedState sharedState, IHAL hal) {
 		super(sharedState, hal);
 	}
-
-	//public static final int DEFAULT_EXPLORATION_ANGLE_DIFF = 5;
+	
+	// Shared constants
 	public static final int LOOP_DELAY = 10;
+	public static final float FORWARD_DISTANCE = 100.0f; // in cm
+
+	// Line-based action constants
 	public static final int SEARCH_COURSE_ANGLE = -45;
-	public static final int STRAIGHT_LINE_THRESHOLD = 1000; // in ms
-	public static final float FORWARD_DISTANCE = 130.0f; // in cm
-
+	public static final float STRAIGHT_LINE_DISTANCE = 10.f; // in cm
+	
+	// Ultrasonic-based action constants
+	private static final int MAX_TURN_ANGLE = 45;
+	// The initial offset angle. Should be in the direction of the sensor.
+	// This avoids that we fall of the other side before reaching the edge.
+	private static final int OFFSET_ANGLE = 45;
+	private static final Speed FORWARD_SPEED = Speed.VeryFast;
+	private static final Speed EDGE_SEARCH_SPEED = Speed.Fast;
+	private static final Speed EDGE_FOLLOW_SPEED = Speed.VeryFast;
+	private static final float DROPOFF_DISTANCE_THRESHOLD = 8.0f; // in cm
+	
 	private boolean suppressed = false;
-
 	private FindLineBehaviour findLineBehav;
 	private Direction lastDirection = Direction.LEFT;
-
+	
 	@Override
 	public void action() {
+		this.ultrasonic_based_action();
+		// alternative implementation: this.line_based_action();
+	}
+	
+	public void ultrasonic_based_action() {
+		this.suppressed = false;
+		
+		this.hal.setSpeed(FORWARD_SPEED);
+		this.hal.resetLeftTachoCount();
+		this.hal.forward();
+		while (!suppressed && this.hal.getLeftTachoDistance() < 10.0f) {
+			Delay.msDelay(10);
+		}
+
+		// RELEASE THE KRAKEN (and wait for it)
+		this.hal.moveDistanceSensorToPosition(DistanceSensorPosition.DOWN);
+
+		// Wait until we have a stable signal. We at least wait for 10
+		// iterations and ensure
+		// that we initially cannot see the dropoff.
+		LCD.drawString("Mode: initializing", 0, 3);
+		while (!this.suppressed && this.canSeeDropoff(this.getDistance())) {
+			Delay.msDelay(LOOP_DELAY);
+		}
+
+		// Configure the follow angle. We use this initially before we have
+		// found the edge
+		// for the first time.
+		LCD.drawString("Mode: finding edge", 0, 3);
+		this.hal.resetGyro();
+		this.hal.setCourseFollowingAngle(OFFSET_ANGLE);
+		this.hal.setSpeed(EDGE_SEARCH_SPEED);
+		while (!this.suppressed) {
+			this.hal.performCourseFollowingStep();
+			if (this.canSeeDropoff(this.getDistance())) {
+				break;
+			}
+			Delay.msDelay(LOOP_DELAY);
+		}
+
+		// Turn to the left until we can barely see the edge anymore and go go
+		// go.
+		Sound.beep();
+		this.hal.setSpeed(EDGE_SEARCH_SPEED);
+		this.hal.rotate(-MAX_TURN_ANGLE);
+		while (!this.suppressed && this.canSeeDropoff(this.getDistance())) {
+			Delay.msDelay(LOOP_DELAY);
+		}
+		this.hal.stop();
+
+		// Our strategy is the following: We just keep going until we reach the
+		// drop-off.
+		// We then start to regulate the motors such that we keep a safe
+		// distance to the drop-off.
+		Sound.buzz();
+		LCD.clear(3);
+		LCD.drawString("Mode: following edge", 0, 3);
+		this.hal.setSpeed(EDGE_FOLLOW_SPEED);
+		while (!this.suppressed && this.hal.getLeftTachoDistance() < FORWARD_DISTANCE) {
+			// Get (filtered) distance
+			float distance = this.getDistance();
+			boolean canSeeDropoff = this.canSeeDropoff(distance);
+
+			// Robot control.
+			if (canSeeDropoff) {
+				// Turn slightly to the left until we do not see the dropoff
+				// anymore.
+				this.hal.turn(-MAX_TURN_ANGLE);
+				while (!this.suppressed && this.hal.isRotating() && !this.hal.getLineType().equals(LineType.LINE)) {
+					if (!this.canSeeDropoff(this.getDistance())) {
+						break;
+					}
+					Delay.msDelay(LOOP_DELAY);
+				}
+			} else {
+				// Turn slightly to the right until we do not see the dropoff
+				// anymore.
+				this.hal.turn(MAX_TURN_ANGLE);
+				while (!this.suppressed && this.hal.isRotating() && !this.hal.getLineType().equals(LineType.LINE)) {
+					if (this.canSeeDropoff(this.getDistance())) {
+						break;
+					}
+					Delay.msDelay(LOOP_DELAY);
+				}
+			}
+			Delay.msDelay(LOOP_DELAY);
+		}
+		this.hal.stop();
+		this.hal.moveDistanceSensorToPosition(DistanceSensorPosition.UP);
+		
+		// Move forward until we meet the line.
+		this.hal.forward();
+		while (!this.suppressed && !this.hal.getLineType().equals(LineType.LINE)) {
+			Delay.msDelay(LOOP_DELAY);
+		}
+		this.hal.stop();
+		this.sharedState.setLineFollowHint(Direction.RIGHT); // first look to the right
+		this.sharedState.setState(MyState.LineFollowState);
+	}
+	
+	private float getDistance() {
+		float distance = this.hal.getMeanDistance();
+		this.hal.printOnDisplay("Distance: " + Float.toString(distance), 3, 0);
+		return distance;
+	}
+
+	private boolean canSeeDropoff(float distance) {
+		boolean canSeeDropoff = (distance > DROPOFF_DISTANCE_THRESHOLD);
+		this.hal.printOnDisplay(canSeeDropoff ? "Dropoff detected" : "No dropoff detected", 3, 0);
+		return canSeeDropoff;
+	}
+
+	public void line_based_action() {
 		this.suppressed = false;
 		
 		this.hal.resetLeftTachoCount();
@@ -44,39 +169,27 @@ public class RockerBehaviour extends StateBehavior {
 		//Delay.msDelay(5000);
 		
 		// At this point, we are on the line. Follow it until we are mostly straight.
-		long currentTime;
-		long lastTimeLineFound = Long.MAX_VALUE;
 		this.hal.printOnDisplay("Line found", 1, 0);
 		this.hal.setSpeed(Speed.Rocker);
 		this.hal.resetGyro();
-		boolean lineFollowDone = false;
-		while (!this.suppressed && !lineFollowDone) {
+		
+		// WARNING: Do not reset the tacho count here since we need the overall distance travelled!
+		float initialDistance = this.hal.getLeftTachoDistance();
+		while (!this.suppressed && this.hal.getLeftTachoDistance() - initialDistance < STRAIGHT_LINE_DISTANCE) {
 			switch (this.hal.getLineType()) {
 			case LINE:
-				currentTime = System.currentTimeMillis();
-				lastTimeLineFound = Math.min(lastTimeLineFound, currentTime);
-
-				if (currentTime - lastTimeLineFound >= STRAIGHT_LINE_THRESHOLD) {
-					// engage full speed mode
-					lineFollowDone = true;
-					this.hal.stop();
-				} else {
-					this.hal.forward();
-				}
+				this.hal.forward();
 				break;
 			case BORDER:
 			case BLACK:
+			default:
 				// still searching a straight way ahead.
-				lastTimeLineFound = Long.MAX_VALUE;
 				this.findLineBehav = new FindLineBehaviour(sharedState, hal, 12,
 						this.lastDirection.getOppositeDirection(), false);
 				this.findLineBehav.action();
 				this.lastDirection = this.findLineBehav.getLastUsedDirection();
-				if (findLineBehav.returnState().equals(LineType.LINE)) {
-					this.hal.forward();
-				}
-				break;
-			default:
+				this.hal.resetLeftTachoCount();
+				this.hal.stop();
 				break;
 			}
 			Delay.msDelay(RockerBehaviour.LOOP_DELAY);
