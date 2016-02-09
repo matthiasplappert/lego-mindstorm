f9db7df3bd097b150c197ca964c54378534079e9
Matthias Plappert <matthiasplappert@me.com>
2016-02-09 14:41:36 +0100

    Remove old implementation

--- src/Behaviors/RockerBehaviour.java
+++ src/Behaviors/RockerBehaviour.java
@@ -17,32 +17,21 @@ public class RockerBehaviour extends StateBehavior {
 	// Shared constants
 	public static final int LOOP_DELAY = 10;
 	public static final float FORWARD_DISTANCE = 100.0f; // in cm
-
-	// Line-based action constants
-	public static final int SEARCH_COURSE_ANGLE = -45;
-	public static final float STRAIGHT_LINE_DISTANCE = 10.f; // in cm
+	private static final float DROPOFF_DISTANCE_THRESHOLD = 8.0f; // in cm
 	
-	// Ultrasonic-based action constants
-	private static final int MAX_TURN_ANGLE = 45;
 	// The initial offset angle. Should be in the direction of the sensor.
 	// This avoids that we fall of the other side before reaching the edge.
 	private static final int OFFSET_ANGLE = 45;
+	private static final int MAX_TURN_ANGLE = 45;
+	
 	private static final Speed FORWARD_SPEED = Speed.VeryFast;
 	private static final Speed EDGE_SEARCH_SPEED = Speed.Fast;
 	private static final Speed EDGE_FOLLOW_SPEED = Speed.VeryFast;
-	private static final float DROPOFF_DISTANCE_THRESHOLD = 8.0f; // in cm
 	
 	private boolean suppressed = false;
-	private FindLineBehaviour findLineBehav;
-	private Direction lastDirection = Direction.LEFT;
 	
 	@Override
 	public void action() {
-		this.ultrasonic_based_action();
-		// alternative implementation: this.line_based_action();
-	}
-	
-	public void ultrasonic_based_action() {
 		this.suppressed = false;
 		
 		this.hal.setSpeed(FORWARD_SPEED);
@@ -150,71 +139,6 @@ public class RockerBehaviour extends StateBehavior {
 		return canSeeDropoff;
 	}
 
-	public void line_based_action() {
-		this.suppressed = false;
-		
-		this.hal.resetLeftTachoCount();
-		this.hal.resetRightTachoCount();
-
-		this.hal.printOnDisplay("RockerBehaviour started", 0, 0);
-		this.hal.printOnDisplay("Searching for line", 1, 0);
-		this.hal.resetGyro();
-		this.hal.setCourseFollowingAngle(SEARCH_COURSE_ANGLE);
-		while (!this.suppressed && !this.hal.getLineType().equals(LineType.LINE)) {
-			this.hal.performCourseFollowingStep();
-			Delay.msDelay(LOOP_DELAY);
-		}
-		Sound.beep();
-		this.hal.stop();
-		//Delay.msDelay(5000);
-		
-		// At this point, we are on the line. Follow it until we are mostly straight.
-		this.hal.printOnDisplay("Line found", 1, 0);
-		this.hal.setSpeed(Speed.Rocker);
-		this.hal.resetGyro();
-		
-		// WARNING: Do not reset the tacho count here since we need the overall distance travelled!
-		float initialDistance = this.hal.getLeftTachoDistance();
-		while (!this.suppressed && this.hal.getLeftTachoDistance() - initialDistance < STRAIGHT_LINE_DISTANCE) {
-			switch (this.hal.getLineType()) {
-			case LINE:
-				this.hal.forward();
-				break;
-			case BORDER:
-			case BLACK:
-			default:
-				// still searching a straight way ahead.
-				this.findLineBehav = new FindLineBehaviour(sharedState, hal, 12,
-						this.lastDirection.getOppositeDirection(), false);
-				this.findLineBehav.action();
-				this.lastDirection = this.findLineBehav.getLastUsedDirection();
-				this.hal.resetLeftTachoCount();
-				this.hal.stop();
-				break;
-			}
-			Delay.msDelay(RockerBehaviour.LOOP_DELAY);
-		}
-		
-		this.hal.stop();
-		LCD.clear();
-		this.hal.printOnDisplay("RockerBehaviour", 0, 0);
-		this.hal.printOnDisplay("going straight ahead", 1, 0);
-		
-		// Just keep going straight until the end of the bridge. We ignore the barcode.
-		this.hal.resetGyro();
-		this.hal.setCourseFollowingAngle(0);
-		this.hal.setSpeed(Speed.VeryFast);
-		while (!this.suppressed && this.hal.getLeftTachoDistance() < FORWARD_DISTANCE) {
-			this.hal.performCourseFollowingStep();
-			this.hal.printOnDisplay("distance: " + this.hal.getLeftTachoCount(), 3, 0);
-			Delay.msDelay(LOOP_DELAY);
-		}
-		this.hal.stop();
-		
-		// Find the line again.
-		this.sharedState.setState(MyState.LineSearchState);
-	}
-
 	@Override
 	MyState getTargetState() {
 		return MyState.RockerState;
@@ -222,9 +146,6 @@ public class RockerBehaviour extends StateBehavior {
 
 	@Override
 	public void suppress() {
-		if (this.findLineBehav != null) {
-			this.findLineBehav.suppress();
-		}
 		suppressed = true;
 	}
 }
