6cf10e96923ad8232c1435a5301a1ee232a7061a
Christian Olms <git@christianolms.de>
2016-02-09 17:01:29 +0100

    fixing some errors

--- src/Behaviors/ElevatorBehaviour.java
+++ src/Behaviors/ElevatorBehaviour.java
@@ -2,6 +2,8 @@ package Behaviors;
 
 import java.io.IOException;
 
+import javax.swing.text.DefaultEditorKit.BeepAction;
+
 import HAL.ColorMode;
 import HAL.DistanceSensorPosition;
 import HAL.IHAL;
@@ -20,7 +22,11 @@ public class ElevatorBehaviour extends StateBehavior {
 	private static final int ANGLE = -30;
 	private static final int MAX_MOVE_ON_PLATOON_DISTANCE = 40;
 	private static final int BACK_DISTANCE_ON_PLATOON = -7;
+	private static final float MIN_DIST = 1.5f;
+	private static final float DISTANCE_TOLERANCE = 0.5f;
+	private static final int TURN_ANGLE = 5;
 	private static final Speed forwardSpeed = Speed.Fast;
+	private static final float MAX_RANGE_PLATOON = 30;
 	private ComModule comm;
 
 	public ElevatorBehaviour(SharedState sharedState, IHAL hal) {
@@ -37,127 +43,171 @@ public class ElevatorBehaviour extends StateBehavior {
 		try {
 			LCD.clear();
 			this.hal.printOnDisplay("ElevatorBehaviour started", 0, 0);
-			
-			
-			while (!this.suppressed && !this.finished) {//HERE IS outer loop!
+
+			while (!this.suppressed && !this.finished) {// HERE IS outer loop!
 				boolean status = false;
-				this.hal.moveDistanceSensorToPosition(DistanceSensorPosition.UP);//Here we do not need Distance Sensor
-				
+				this.hal.moveDistanceSensorToPosition(DistanceSensorPosition.Labyrinth);// Here
+																					// we
+																					// do
+																					// not
+																					// need
+																					// Distance
+																					// Sensor
 
 				this.hal.resetLeftTachoCount();
 				this.hal.resetGyro();
 				this.hal.setSpeed(Speed.Fast);
 				this.hal.setCourseFollowingAngle(ANGLE);
-				while(this.hal.getLeftTachoDistance() < MAX_MOVE_ON_PLATOON_DISTANCE &&
-						!this.hal.isTouchButtonPressed()&& !this.suppressed){//move forward to stand on lightened platoon
+				while (this.hal.getLeftTachoDistance() < MAX_MOVE_ON_PLATOON_DISTANCE
+						&& !this.hal.isTouchButtonPressed() && !this.suppressed) {// move
+																					// forward
+																					// to
+																					// stand
+																					// on
+																					// lightened
+																					// platoon
 					this.hal.performCourseFollowingStep();
 
-						Delay.msDelay(10);					
-				}
-				this.hal.stop();
-				//move back
-				go_back(BACK_DISTANCE_ON_PLATOON);
-				this.hal.rotate(Math.abs(ANGLE));
-				while(this.hal.isRotating() && !this.suppressed){
 					Delay.msDelay(10);
 				}
 				this.hal.stop();
-				
-				while (status == false && !this.suppressed )				
-				{//wait for status=true
+				// move back
+				go_back(BACK_DISTANCE_ON_PLATOON);
+				this.hal.rotate(Math.abs(ANGLE));
+				while (this.hal.isRotating() && !this.suppressed) {
+					Delay.msDelay(10);
+				}
+				this.hal.stop();
+
+				while (status == false && !this.suppressed) {// wait for
+																// status=true
 
 					status = this.comm.requestStatus();
-					LCD.drawString("status is false       " , 1, 0);
+					LCD.drawString("status is false       ", 1, 0);
 					Delay.msDelay(100);
 
 				}
 				this.hal.setColorMode(ColorMode.AMBIENT_LIGHT);
-				LCD.drawString("Request Elevator       " , 1, 0);
+				LCD.drawString("Request Elevator       ", 1, 0);
 
-				if(this.comm.requestElevator()){//reserve elevator
+				if (this.comm.requestElevator()) {// reserve elevator
 
-					//wait for safe signal
+					// wait for safe signal
 					this.wait_for_ambient_light_on();
-					//enter elevator
-					LCD.drawString("Ambient Light off      " , 1, 0);
-					this.move_forward_till_button();
+					// enter elevator
+					LCD.drawString("Ambient Light off      ", 1, 0);
+					this.followWallUntilElevatorEnd();
 					this.hal.setColorMode(ColorMode.RED);
-					//request elevator to move
-					LCD.drawString("Request Elevator       " , 1, 0);
-					LCD.drawString("to go down       " , 2, 0);
+					// request elevator to move
+					LCD.drawString("Request Elevator       ", 1, 0);
+					LCD.drawString("to go down       ", 2, 0);
+					Sound.beep();
+					if (this.comm.moveElevatorDown()) {
+						Delay.msDelay(ELEVATOR_MOVING_DURATION * 1000);
+						LCD.drawString("Try to leave Elevator      ", 1, 0);
+						LCD.drawString("                           ", 2, 0);
 
-					if(this.comm.moveElevatorDown()){
-						Delay.msDelay(ELEVATOR_MOVING_DURATION*1000);
-						LCD.drawString("Try to leave Elevator      " , 1, 0);
-						LCD.drawString("                           " ,2, 0);
-						
 						this.move_until_line();
 						finished = true;
 						this.sharedState.reset(true);
-//						new ObstacleEndBehavior(this.sharedState, this.hal).action();
+						// new ObstacleEndBehavior(this.sharedState,
+						// this.hal).action();
+					} else {
+						// TODO: Move back to safe position and go to outer loop
+						throw new IllegalStateException("Error in elevator down!");
+						// break;//goto outer loop
 					}
-					else{
-						//TODO: Move back to safe position and go to outer loop
-						throw new IllegalStateException("we hang in elevator!");
-//						break;//goto outer loop
-					}
+				} else {
+					break;// go to outer loop
 				}
-				else{
-					break;//go to outer loop
-				}
-				
+
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-//		Thread.yield();
+		// Thread.yield();
 
 	}
 
 	private void move_until_line() {
 		this.hal.setSpeed(forwardSpeed);
 		this.hal.forward();
-		while(this.hal.getLineType() != LineType.LINE && !this.suppressed){
+		while (this.hal.getLineType() != LineType.LINE && !this.suppressed) {
 			LCD.drawString("No Line Found", 1, 0);
 			Delay.msDelay(10);
 		}
 		this.hal.stop();
-		
+
 	}
 
-	private void move_forward_till_button() {
+//	private void move_forward_till_button() {
+//
+//		// get close to the wall
+//		this.hal.forward();
+//		while (!this.hal.isTouchButtonPressed() && !this.suppressed) {
+//			Delay.msDelay(10);
+//		}
+//		go_back(-2);
+//
+//	}
 
-		//get close to the wall
-		this.hal.forward();
-		while (!this.hal.isTouchButtonPressed() && !this.suppressed) {
+	private void followWallUntilElevatorEnd() {
+		
+		Sound.beep();
+		this.hal.setSpeed(Speed.Slow);
+		
+		while(!this.suppressed && this.hal.getMeanDistance()> MAX_RANGE_PLATOON){
+			this.hal.forward();
 			Delay.msDelay(10);
 		}
-		go_back(-2);
+		
+		while (!this.suppressed && this.hal.isTouchButtonPressed()) {
+			// Get (filtered) distance
+			float distance = this.hal.getMeanDistance();
 
+			this.hal.printOnDisplay("dist to wall: " + distance, 1, 0);
+
+			// Keep distance to wall.
+			if (distance > MIN_DIST+ DISTANCE_TOLERANCE) {
+				this.hal.turn(TURN_ANGLE);
+			}
+			
+			
+			else if (distance < MIN_DIST) {
+				this.hal.turn(-TURN_ANGLE);
+			} 
+			else {
+				this.hal.forward();
+			}
+			Delay.msDelay(10);
+
+		}
+		this.hal.stop();
+		this.hal.setSpeed(forwardSpeed);
 	}
 
 	private void go_back(int distance) {
 		Sound.beep();
-		int neg_dist = -1* Math.abs(distance);
+		int neg_dist = -1 * Math.abs(distance);
 		this.hal.stop();
 		this.hal.setSpeed(Speed.Medium);
 		this.hal.resetLeftTachoCount();
 		this.hal.backward();
-		
-		while(this.hal.getLeftTachoDistance() > neg_dist && !this.suppressed){
+
+		while (this.hal.getLeftTachoDistance() > neg_dist && !this.suppressed) {
 			Delay.msDelay(10);
 		}
-		this.hal.stop();	
+		this.hal.stop();
 		this.hal.setSpeed(forwardSpeed);
 	}
 
 	private void wait_for_ambient_light_on() {
-		while(!this.hal.isAmbientLightOn()&& !this.suppressed){
-			LCD.drawString("Ambient Light is off       " , 1, 0);
+		while (!this.hal.isAmbientLightOn() && !this.suppressed) {
+			LCD.drawString("Ambient Light is off       ", 1, 0);
 			Delay.msDelay(10);
 		}
-		
+
 	}
 
 	@Override
