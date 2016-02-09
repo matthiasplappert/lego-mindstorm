46e352c5ccecff3982a6598ea60d4c4a83cdbe4c
Christian Olms <git@christianolms.de>
2016-02-09 19:28:17 +0100

    fix directions

--- src/Behaviors/ElevatorBehaviour.java
+++ src/Behaviors/ElevatorBehaviour.java
@@ -17,7 +17,7 @@ import State.MyState;
 
 public class ElevatorBehaviour extends StateBehavior {
 
-	private static final int ELEVATOR_MOVING_DURATION = 10;
+	private static final int ELEVATOR_MOVING_DURATION = 8;
 	private static final int ANGLE = -35;
 	private static final int MAX_MOVE_ON_PLATOON_DISTANCE = 80;
 	private static final int BACK_DISTANCE_ON_PLATOON = -10;
@@ -158,12 +158,12 @@ public class ElevatorBehaviour extends StateBehavior {
 
 			// Keep distance to wall.
 			if (distance > MIN_DIST+ DISTANCE_TOLERANCE) {
-				this.hal.turn(-TURN_ANGLE);
+				this.hal.turn(TURN_ANGLE);
 			}
 			
 			
 			else if (distance < MIN_DIST) {
-				this.hal.turn(TURN_ANGLE);
+				this.hal.turn(-TURN_ANGLE);
 			} 
 			else {
 				this.hal.forward();
